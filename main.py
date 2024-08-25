795faf87da902e11ae9634994d19d31316c0184b
Jefferson <jeffersonamorim258@gmail.com>
2024-08-25 15:49:36 -0300

    Realizado os graus de centralidade e intermediação

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 Módulo principal.
 """
 
-from utils import formata_numero
 from pathlib import Path
 import pandas as pd
 import networkx as nx
+from utils import formata_numero
 
 PASTA_RAIZ = Path(__file__).parent
 BASE_DE_DADOS = 'contratos.csv'
@@ -42,4 +42,44 @@ for indice, linha in df.iterrows():
 
     G.add_edge(orgao_contratante, nome_empresa, weight=valor_contrato)
 
-nx.write_gexf(G, "grafo.gexf")
+# nx.write_gexf(G, "grafo.gexf")
+
+empresas = df['NOME DO FORNECEDOR'].unique()
+
+centralidade = nx.degree_centrality(G)
+intermediacao = nx.betweenness_centrality(G)
+
+centralidade_empresas = {e: centralidade[e]
+                         for e in empresas if e in centralidade}
+
+intermediacao_empresas = {e: intermediacao[e]
+                          for e in empresas if e in intermediacao}
+
+print("quantidade de empresas: ", len(empresas))
+
+media_centralidade = sum(centralidade_empresas.values()
+                         ) / len(centralidade_empresas)
+
+media_intermediacao = sum(intermediacao_empresas.values()
+                          ) / len(intermediacao_empresas)
+
+acima_media_centralidade = {e: p for e, p in centralidade_empresas.items()
+                            if p > media_centralidade}
+
+acima_media_intermediacao = {e: p for e, p in intermediacao_empresas.items()
+                             if p > media_intermediacao}
+
+print('quantidade empresas acima da media em centralidade: ',
+      len(acima_media_centralidade))
+
+print('quantidade empresas acima da media em intermediacao: ',
+      len(acima_media_intermediacao))
+
+empresas_resultantes_acima = set(acima_media_centralidade.keys()) & set(
+    acima_media_intermediacao.keys())
+
+print('Quantidade empresas na intersecção: ',
+      len((empresas_resultantes_acima)))
+
+for e in empresas_resultantes_acima:
+    print(e)
